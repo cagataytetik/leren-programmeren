d0af86133b11fcbe9d2cf6c44caeb35a3135f469
Cagatay Tetik <99072323@mydavinci.nl>
2022-09-30 10:12:07 +0200

    person1.py

---
+++ module-2/person.py
@@ -0,0 +1,11 @@
+naam = input("wat is je naam:")
+adres = input("wat is je adres?:")
+postcode = input("wat is je adres?:")
+woonplaats = input("wat is je woonplaats?:")
+
+print ("----------------------------------------------------")
+print (f" naam:{naam}")
+print (f" adres:{adres}")
+print (f" postcode:{postcode}")
+print (f" woonplaats:{woonplaats}")
+print ("----------------------------------------------------")
