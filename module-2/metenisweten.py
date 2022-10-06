c007acc33633872800d992cd505de3cd88985bab
Cagatay Tetik <99072323@mydavinci.nl>
2022-10-06 09:54:58 +0200

    edit

---
+++ module-2/metenisweten.py
@@ -0,0 +1,16 @@
+a = int(input("geef een getal:"))
+b = int(input("geef een getal:"))
+
+max = a
+min = b
+
+if a > b:
+    print (f"a is het grootste getal: {max}")
+
+elif a < b:
+    print (f" b is het grootste getal: {min}")
+else:
+    print (f"a en b zijn even groot")
+
+    print (f"het minimun is {min}")
+    print (f"het maximum is {max}")
