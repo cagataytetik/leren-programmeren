7d9596e380970adadd334d4059a473275764e7ff
Cagatay Tetik <99072323@mydavinci.nl>
2022-12-12 15:53:06 +0100

    flowchart-game

--- module-2/game.py
+++ module-2/game.py
@@ -1,10 +1,9 @@
-
 naam = input("Wat is jou naam?: ")
 
-ready = input("Ben je ready om een avontuur te beleven? (ja of nee)?: ").lower()
+ready = input("Ben je ready om een avontuur te beleven? (ja of nee)?: ")
 
 if ready == "ja":
-    vraag1 = input(f"Oke {naam} naar waar wil je toe A(Woestijn, B(Bergen, C(Bos: ").lower()
+    vraag1 = input(f"Oke {naam} naar waar wil je toe A(Woestijn, B(Bergen, C(Bos: ")
 else:
     print("Watje...")
     exit()
@@ -13,7 +12,7 @@ if vraag1 == 'a':
     print("Je ging zonder water naar de woestijn je sterfte uiteindelijk van de dorst....")
     exit()
 elif vraag1 == "b":
-    vraag2 = input("Je klimt op de berg het begint steeds kouder te worden... Je ziet een een grot.. A(in de grot lopen? B(verder lopen?: ").lower()
+    vraag2 = input("Je klimt op de berg het begint steeds kouder te worden... Je ziet een een grot.. A(in de grot lopen? B(verder lopen?: ")
     if vraag2 == "a":
         print("Je werd door een beer opgegeten in de grot R.I.P.....")
         exit()
@@ -25,9 +24,9 @@ elif vraag1 == "b":
         else:
             print("Waarom ging je niet naar het huisje? je friest nu dood... R.I.P")
 else:
-    vraag4 = input("Je loopt nu door de bos.. het word best donker... A(Vuurtje maken en ergens slapen B(Door lopen?").lower()
+    vraag4 = input("Je loopt nu door de bos.. het word best donker... A(Vuurtje maken en ergens slapen B(Door lopen?")
     if vraag4 == "a":
-       vraag5 = input("Je bent voor hout aan t zoeken je hoort rare geluiden ga je erop af? (ja of nee)?: ").lower()
+       vraag5 = input("Je bent voor hout aan t zoeken je hoort rare geluiden ga je erop af? (ja of nee)?: ")
        if vraag5 == "ja":
             print("Je vind een portaal je stapt erin en hij brengt je thuis gefeliciteerd!!")
             exit() 
