749b4452f69d59a4770978c54a088b15434a352f
Cagatay Tetik <99072323@mydavinci.nl>
2023-01-01 16:32:43 +0100

    example-12

---
+++ module-3/forever-young/robotarm-python-2021-main/example-12.py
@@ -0,0 +1,19 @@
+from RobotArm import RobotArm
+robotArm = RobotArm('exercise 12')
+color = "red"
+rechts = 9
+links = 9
+for b in range(9):
+    robotArm.grab()
+    if robotArm.scan() == color:
+        for f in range(rechts-b):
+            robotArm.moveRight()
+        robotArm.drop()
+        for t in range(b):
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+    robotArm.moveRight()
+    
+
+robotArm.wait()
