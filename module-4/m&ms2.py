b2480ad61debeb33e9e65094ffcb502587b88125
Cagatay Tetik <99072323@mydavinci.nl>
2023-01-31 14:43:44 +0100

    mms

--- module-4/m&ms2.py
+++ module-4/m&ms2.py
@@ -1,12 +1,10 @@
 import random
-kleurLijst = ["rood", "blauw", "groen", "geel", "bruin"]
-mms = int(input("Hoeveel M&M's moeten er gevoegd worden: "))
-
-zak = {}
 getal = 1
-
+kleuren = ["rood", "blauw", "groen", "geel", "bruin"]
+mms = int(input("Hoeveel M&M's moeten er gevoegd worden: "))
+zak = {}
 for a in range(mms):
-    kleur = random.choice(kleurLijst)
+    kleur = random.choice(kleuren)
     if kleur not in zak:   
         zak.update({kleur: getal})
     elif kleur in zak:  
