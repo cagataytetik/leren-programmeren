0cf0dce286175ba9d15dc82075cd6a230587c0fa
Cagatay Tetik <99072323@mydavinci.nl>
2023-02-14 11:17:16 +0100

    edit

--- module-4/m&ms.py
+++ module-4/m&ms.py
@@ -1,5 +1,8 @@
 import random
+zak = []
 M_en_Ms = ("oranje", "blauw", "groen", "bruin","paars")
 vraag = int(input("Hoeveel M&Ms er aan de zak toegevoegd worden?: "))
+
 for u in range(vraag):
-    print(random.choice(M_en_Ms))
+    zak.append(random.choice(M_en_Ms))
+print (zak)
