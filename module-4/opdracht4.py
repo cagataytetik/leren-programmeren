4e0a726fd2b50cfae7f612e07a45fff40028e2fc
Cagatay Tetik <99072323@mydavinci.nl>
2023-02-24 14:44:08 +0100

    öpdracht4

--- module-4/opdracht4.py
+++ module-4/opdracht4.py
@@ -3,12 +3,12 @@ import random
 
 aantal = int(input("Hoeveel fruit wilt u?: "))
 
-fruiten = [a['name'] for a in fruitmand if 'name' in a]
+fruiten = [b['name'] for b in fruitmand if 'name' in b]
 
 fruit = (random.choices(fruiten, k=aantal))
 
-for b in fruit:
-    print(b)
+for c in fruit:
+    print(c)
 
 
 
